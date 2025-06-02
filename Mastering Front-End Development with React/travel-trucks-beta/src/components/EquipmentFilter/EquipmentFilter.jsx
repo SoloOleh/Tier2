2c3942702a1604042208042db8bff991cde0f856
Oleh Solomko <solo.oleg1987@gmail.com>
2025-06-02 23:24:30 +0300

    02.06.2025

--- Mastering Front-End Development with React/travel-trucks-beta/src/components/EquipmentFilter/EquipmentFilter.jsx
+++ Mastering Front-End Development with React/travel-trucks-beta/src/components/EquipmentFilter/EquipmentFilter.jsx
@@ -1,3 +1,43 @@
+// import { useDispatch, useSelector } from "react-redux";
+// import Icon from "../Icon/Icon";
+// import { toggleEquipment } from "../../redux/filters/filtersSlice";
+// import { selectEquipment } from "../../redux/filters/selectors";
+// import { VEHICLE_EQUIPMENT } from "../../utils/constants";
+// import styles from "./EquipmentFilter.module.css";
+
+// const EquipmentFilter = () => {
+//   const dispatch = useDispatch();
+//   const equipment = useSelector(selectEquipment);
+
+//   const handleToggle = (value) => {
+//     dispatch(toggleEquipment(value));
+//   };
+
+//   return (
+//     <div className={styles.equipmentFilter}>
+//       <h4 className={styles.subtitle}>Vehicle equipment</h4>
+//       <div className={styles.grid}>
+//         {VEHICLE_EQUIPMENT.map(({ value, label, icon }) => (
+//           <button
+//             key={value}
+//             type="button"
+//             onClick={() => handleToggle(value)}
+//             className={`${styles.option} ${
+//               equipment[value] ? styles.active : ""
+//             }`}
+//           >
+//             <Icon name={icon} size={32} />
+//             <span>{label}</span>
+//           </button>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default EquipmentFilter;
+
+// src/components/EquipmentFilter/EquipmentFilter.jsx
 import { useDispatch, useSelector } from "react-redux";
 import Icon from "../Icon/Icon";
 import { toggleEquipment } from "../../redux/filters/filtersSlice";
@@ -15,7 +55,8 @@ const EquipmentFilter = () => {
 
   return (
     <div className={styles.equipmentFilter}>
-      <h4 className={styles.subtitle}>Vehicle equipment</h4>
+      <h3 className={styles.title}>Vehicle equipment</h3>
+      <div className={styles.divider} />
       <div className={styles.grid}>
         {VEHICLE_EQUIPMENT.map(({ value, label, icon }) => (
           <button
@@ -25,6 +66,7 @@ const EquipmentFilter = () => {
             className={`${styles.option} ${
               equipment[value] ? styles.active : ""
             }`}
+            aria-pressed={equipment[value]}
           >
             <Icon name={icon} size={32} />
             <span>{label}</span>
--- Mastering Front-End Development with React/travel-trucks-beta/src/components/FilterPanel/FilterPanel.jsx
+++ Mastering Front-End Development with React/travel-trucks-beta/src/components/FilterPanel/FilterPanel.jsx
@@ -1,57 +1,17 @@
-// import { useDispatch } from "react-redux";
+// import { useSelector } from "react-redux";
+// import PropTypes from "prop-types";
 // import LocationFilter from "../LocationFilter/LocationFilter";
 // import EquipmentFilter from "../EquipmentFilter/EquipmentFilter";
 // import VehicleTypeFilter from "../VehicleTypeFilter/VehicleTypeFilter";
 // import Button from "../Button/Button";
-// import { resetCampers } from "../../redux/campers/campersSlice";
-// import styles from "./FilterPanel.module.css";
-
-// const FilterPanel = () => {
-//   const dispatch = useDispatch();
-
-//   const handleSearch = () => {
-//     dispatch(resetCampers());
-//   };
-
-//   return (
-//     <div className={styles.filterPanel}>
-//       <LocationFilter />
-
-//       <div className={styles.divider} />
-
-//       <h3 className={styles.title}>Filters</h3>
-
-//       <EquipmentFilter />
-
-//       <VehicleTypeFilter />
-
-//       <Button onClick={handleSearch} fullWidth size="large">
-//         Search
-//       </Button>
-//     </div>
-//   );
-// };
-
-// export default FilterPanel;
-
-// import { useDispatch, useSelector } from "react-redux";
-// import LocationFilter from "../LocationFilter/LocationFilter";
-// import EquipmentFilter from "../EquipmentFilter/EquipmentFilter";
-// import VehicleTypeFilter from "../VehicleTypeFilter/VehicleTypeFilter";
-// import Button from "../Button/Button";
-// import { resetCampers } from "../../redux/campers/campersSlice";
-// import { fetchCampers } from "../../redux/campers/operations";
 // import { selectActiveFilters } from "../../redux/filters/selectors";
 // import styles from "./FilterPanel.module.css";
 
-// const FilterPanel = () => {
-//   const dispatch = useDispatch();
+// const FilterPanel = ({ onApplyFilters }) => {
 //   const filters = useSelector(selectActiveFilters);
 
 //   const handleSearch = () => {
-//     console.log("Search button clicked with filters:", filters);
-//     dispatch(resetCampers());
-//     dispatch(fetchCampers({ page: 1, filters }));
+//     onApplyFilters(filters);
 //   };
 
 //   return (
@@ -73,48 +33,13 @@
 //   );
 // };
 
-// export default FilterPanel;
-
-// import { useDispatch, useSelector } from "react-redux";
-// import LocationFilter from "../LocationFilter/LocationFilter";
-// import EquipmentFilter from "../EquipmentFilter/EquipmentFilter";
-// import VehicleTypeFilter from "../VehicleTypeFilter/VehicleTypeFilter";
-// import Button from "../Button/Button";
-// import { resetCampers } from "../../redux/campers/campersSlice";
-// import { fetchCampers } from "../../redux/campers/operations";
-// import { selectActiveFilters } from "../../redux/filters/selectors";
-// import styles from "./FilterPanel.module.css";
-
-// const FilterPanel = () => {
-//   const dispatch = useDispatch();
-//   const filters = useSelector(selectActiveFilters);
-
-//   const handleSearch = () => {
-//     dispatch(resetCampers());
-//     dispatch(fetchCampers({ page: 1, filters }));
-//   };
-
-//   return (
-//     <div className={styles.filterPanel}>
-//       <LocationFilter />
-
-//       <div className={styles.divider} />
-
-//       <h3 className={styles.title}>Filters</h3>
-
-//       <EquipmentFilter />
-
-//       <VehicleTypeFilter />
-
-//       <Button onClick={handleSearch} fullWidth size="large">
-//         Search
-//       </Button>
-//     </div>
-//   );
+// FilterPanel.propTypes = {
+//   onApplyFilters: PropTypes.func.isRequired,
 // };
 
 // export default FilterPanel;
 
+// src/components/FilterPanel/FilterPanel.jsx
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
 import LocationFilter from "../LocationFilter/LocationFilter";
@@ -132,21 +57,15 @@ const FilterPanel = ({ onApplyFilters }) => {
   };
 
   return (
-    <div className={styles.filterPanel}>
+    <aside className={styles.filterPanel}>
       <LocationFilter />
-
-      <div className={styles.divider} />
-
-      <h3 className={styles.title}>Filters</h3>
-
       <EquipmentFilter />
-
       <VehicleTypeFilter />
 
-      <Button onClick={handleSearch} fullWidth size="large">
+      <Button onClick={handleSearch} fullWidth size="large" variant="primary">
         Search
       </Button>
-    </div>
+    </aside>
   );
 };
 
